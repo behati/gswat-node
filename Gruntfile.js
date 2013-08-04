27727c805263cea74894c1c997a3c6b67d31aff1
Enzo Martin <enzo.r.martin@gmail.com>
2013-08-04 20:47:59 +0200

    [TASK] - Added grunt - Forgot to add jQuery UI stuff - Moved structure to scss

---
+++ Gruntfile.js
@@ -0,0 +1,133 @@
+var fs = require('fs');
+
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'scss',
+					cssDir: 'public/css',
+					fontsDir: 'public/fonts',
+					specify: ['scss/structure.scss'],
+					javascriptsDir: 'public/js',
+					imagesDir: 'public/img',
+					outputStyle: 'compressed',
+					clean: true,
+					environment: 'production'
+				}
+			},
+			dev: {
+				options: {
+					sassDir: 'scss',
+					cssDir: 'public/css',
+					fontsDir: 'public/fonts',
+					specify: ['scss/structure.scss'],
+					javascriptsDir: 'public/js',
+					imagesDir: 'public/img',
+					outputStyle: 'expanded',
+					relativeAssets: true,
+					environment: 'development'
+				}
+			}
+		},
+		jade: {
+			compile: {
+				options: {
+					data: function(){
+						var files = {};
+						files.views = fs.readdirSync('public/js/views');
+						files.models = fs.readdirSync('public/js/models');
+						var jade_template = '';
+
+						for (var i in files) {
+							var j = 0;
+
+							while(j < files[i].length){
+								var file = files[i][j];
+								jade_template += '\'/js/' + i + '/' + file + '\'';
+								j++;
+
+								//TODO: Fix this properly, too tired
+								if(files[i][j]){
+									jade_template += ',';
+								} else {
+									if(i == 'views'){
+										jade_template += ',';
+									}
+								}
+								jade_template += '\n';
+							}
+						}
+						return {files:jade_template};
+					}
+				},
+				files: {
+					'public/js/adminloader.js': 'views/adminloader.jade'
+				}
+			}
+		},
+		watch: {
+			scripts: {
+				files: [
+					'scss/**',
+					'views/adminloader',
+					'views/templates/**',
+					'public/js/views/**',
+					'public/js/models/**'
+				],
+				tasks: [
+					'compass:dev',
+					'build_includes',
+					'jade',
+					'watch'
+				],
+				options: {
+					nospawn: true
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-develop');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+
+	// Add the JADE includes
+	grunt.registerTask('build_includes', 'Generates the JADE mustache template includes', function(){
+		var files = fs.readdirSync('views/templates');
+		var jade_template = '';
+		var i = 0;
+		while (i < files.length) {
+			var file = files[i].replace('.jade','');
+			jade_template += 'script#' + file + '(type="text/html")\n';
+			jade_template += '	include templates/' + file + ' \n \n';
+			i++;
+		}
+		fs.writeFileSync('views/templates.jade', jade_template);
+		return true;
+	});
+
+	// Default development task
+	grunt.registerTask('default', function(){
+		var tasks = [
+			'compass:dev',
+			'build_includes',
+			'jade',
+			'watch'
+		];
+
+		grunt.option('force', true);
+		grunt.task.run(tasks);
+	});
+
+	// Production task
+	grunt.registerTask('release', [
+		'compass:dist',
+		'build_includes',
+		'jade'
+	]);
+};
--- server.js
+++ server.js
@@ -4,10 +4,24 @@
 var express = require('express');
 var passport = require('passport');
 var AzureStorage = require('./core/data/AzureStorage');
+var cp = require('child_process');
+var env = process.env.NODE_ENV || 'development';
+var grunt = null;
+if(env == 'release'){
+	console.log('Running Grunt in release mode');
+	grunt = cp.exec('grunt',['release']);
+} else {
+	console.log('Running Grunt in development mode');
+	grunt = cp.exec('grunt',['default']);
+}
+
+grunt.stdout.on('data', function(data) {
+	// relay grunt output to console
+	console.log("%s", data)
+});
 
 
 // Load configurations
-var env = process.env.NODE_ENV || 'development';
 var config = require('./config/config')[env];
 var auth = require('./config/middlewares/authorization');
 
@@ -25,5 +39,4 @@ require('./config/routes')(app,passport,auth);
 // Start the app by listening on <port>
 var port = process.env.PORT || 3000;
 app.listen(port);
-console.log('GSWAT-node started on ' + port);
-
+console.log('Server started on ' + port);
